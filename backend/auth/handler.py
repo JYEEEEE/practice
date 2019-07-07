bad11dc0584b96bb478a4afae4ed30c969940f08
zhaoyanz405 <zhaoyanz405@gmail.com>
2019-07-07 19:11:06 +0800

    add define

---
+++ backend/auth/handler.py
@@ -0,0 +1,20 @@
+"""
+# todo
+实现注册和登录
+
+"""
+
+
+class Register():
+    """
+    input: {'login_name': xxxx, 'login_passwd': xxxx}
+    """
+
+
+class Login():
+    """
+    input: {'login_name': xxxx, 'login_passwd': xxxx}
+    do: 去数据库查询，有没有该用户， 密码对不对
+    return: code: -1 , 没有改用户， -2 密码不对
+    """
+    
--- db/test.py
+++ db/test.py
@@ -8,4 +8,12 @@ table = db.test  #  db.test
 
 table.insert_many([{'name':'zy','age':'70'},{'name':'wjj','age':'18'}])
 
-print(table.find_one({'age':'18'})) 
+result = table.find_one({'age':'17'})
+
+if not result:
+    # result : None, 0, False, [], {}
+    print('no')
+
+if result is None:
+    # result : None
+    print('no 2')
